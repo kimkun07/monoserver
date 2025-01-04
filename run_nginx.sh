9d3708028464ada53fbf5443fb11f36b2230c242
kimkun07 <kimkun07@naver.com>
2025-01-04 15:53:05 +0900

    chore

--- run_nginx.sh
+++ run_nginx.sh
@@ -1,12 +1,12 @@
-# Stop and remove the container
+# Stop and remove the container if it exists
 docker stop nginx 1>/dev/null 2>/dev/null 
 docker rm nginx 1>/dev/null 2>/dev/null
 
 # Run the container
 # mount: ./nginx/static/index.html will be /usr/share/nginx/html/index.html
 docker run --name nginx \
-       --detach --publish 8080:80 \
+       --detach --publish 80:80 \
        --mount type=bind,src=./nginx/nginx.conf,dst=/etc/nginx/nginx.conf,ro \
-       --mount type=bind,src=./nginx/static,dst=/usr/share/nginx/html,ro \
+       --mount type=bind,src=./display,dst=/usr/share/nginx/html,ro \
        --mount type=bind,src=./services,dst=/data/services,ro \
        nginx:1.27.3
